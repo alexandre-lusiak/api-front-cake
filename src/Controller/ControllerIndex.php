30d01ba0b28d059b7c4e2d363f3f549aef1a158d
YokiSimplon <alekieffre@gmail.com>
2022-08-31 20:37:30 +0200

    add entity adress

--- src/Controller/ControllerIndex.php
+++
@@ -1,9 +0,0 @@
-#[Route('/blog/{slug}', name: 'blog_show')]
-public function show(string $slug): Response
-{
-// $slug will equal the dynamic part of the URL
-// e.g. at /blog/yay-routing, then $slug='yay-routing'
-
-// ...
-}
-}
